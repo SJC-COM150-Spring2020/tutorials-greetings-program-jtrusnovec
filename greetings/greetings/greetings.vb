8bd6717d8bc6af90d97e26130c8bc62a6bd0e717
jtrusnovec <60245181+jtrusnovec@users.noreply.github.com>
2020-02-06 20:26:03 -0500

    printed greetings to listbox and added picture box
    used listbox to print value of txtName to lstOut. added clear and exit buttongs. added picGreetings.

--- greetings/greetings/greetings.vb
+++ greetings/greetings/greetings.vb
@@ -1,15 +1,25 @@
 ﻿Public Class greetings
     Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
 
-        'take value from name textbox and print in messagebox
-        'MessageBox.Show(txtName.Text)
+        'print Greetings to listbox
+        lstOut.Items.Add("Good Evening " & txtName.Text)
+        lstOut.Items.Add("How are you?")
 
-        'take value from name textbox and print to txtOut
-        txtOut.Text = "Good Evening " & txtName.Text & ControlChars.CrLf
-        txtOut.Text = txtOut.Text & " How are you?"
+
+
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
 
         'clear txtName value
         txtName.Clear()
+        lstOut.Items.Clear()
+    End Sub
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'when user exits the program
+        Beep()
+        Application.Exit()
     End Sub
 End Class
