3d79c1009f534e0e6ef5bd51f931284a3060c175
jtrusnovec <60245181+jtrusnovec@users.noreply.github.com>
2020-02-06 19:13:27 -0500

    print greetings to messagebox
    print greetings message using messagebox

---
+++ greetings/greetings/greetings.vb
@@ -0,0 +1,9 @@
+﻿Public Class greetings
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+
+        'take value from name textbox and print in messagebox
+        MessageBox.Show(txtName.Text)
+
+
+    End Sub
+End Class
